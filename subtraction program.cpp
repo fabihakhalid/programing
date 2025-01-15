203653208ebf2eb4e450ac60316551041bc17b4a
fabiha khalid <fabihakhalid2005@gmail.com>
2025-01-15 11:47:09 -0800

    done by fabiha

---
+++ subtraction program.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main () {
+	int a,b,sub;
+	
+    cout<<"enter a number :" ;
+    cin>>a;
+    
+    cout<<"enter a number :" ;
+    cin>>b;
+    
+    sub=a-b;
+    cout<<"the num is"<<sub<<endl;
+    
+    return 0;
+}
